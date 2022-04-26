571054063517297a84d5fb3ef72c9d81a32314d3
Neelam <neelam0101singh@gmail.com>
2022-04-26 22:27:18 +0530

    added

---
+++ day9_auth_context/src/App.jsx
@@ -0,0 +1,16 @@
+import { Navbar } from './components/navbar'
+import User from "./components/Users"
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      < Navbar/>
+      <User></User>
+    </div>
+  )
+}
+
+export default App
